f8ef127bd7e6322069e0509962b9a9db4bc48fd5
bcoe <=>
2012-08-22 02:47:57 -0400

    Updated description.

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 
 setup(name="sampy",
       version="0.0.1",
-      description="Perodically sample arbitrary data. Backed by Redis. Ganglia plugin included.",
+      description="Redis-backed tool for probabilistically tracking the throughput of distributed systems.",
       author="Benjamin Coe",
       author_email="ben@attachments.me",
       entry_points = {},
-      url="https://github.com/attachmentsme/sampy",
+      url="https://github.com/bcoe/sampy",
       packages = find_packages(),
       install_requires = ['redis'],
       tests_require=['nose']
